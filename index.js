ea345d6e4531918b6f8decebd17e9ca1661262fb
Jirasak07 <bee.110243@gmail.com>
2022-11-18 20:19:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get('/',function(req,res,next){
+    res.send("API is Run!!");
+})
+app.listen(2222, function () {
+    console.log("CORS PORT 2222");
+  });
+  module.exports = app;
