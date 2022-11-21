f5109602bffb27436ffb8ace36e84f8c089d4070
Jirasak07 <bee.110243@gmail.com>
2022-11-21 09:32:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var imgconfig = multer.diskStorage({
   })
 app.use(function(req,res,next){
       // Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+      res.setHeader('Access-Control-Allow-Origin', '*');
 
       // Request methods you wish to allow
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
